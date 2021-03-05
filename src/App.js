78a93a45833bc1a77b699720268854dce55b90c0
iamniama <plectrumonthespectrum@gmail.com>
2021-03-05 11:15:27 -0700

    update navbar component and add routes to APP

--- src/App.js
+++ src/App.js
@@ -15,18 +15,66 @@ import Welcome from "./components/Welcome";
 import './App.css';
 
 // Components
+const PrivateRoute = ({component: Component, ...rest}) => {
+    let token = localStorage.getItem('jwt-token')
+    console.log("=====> Private Route mode engage")
+    return <Route {...rest} render={(props) => {
+        return token ? <Component {...rest} {...props} /> : <Redirect to={"/login"} />
+    } } />
+}
 
 function App() {
-  // Set state values
+    const [currentUser, setCurrentUser] = useState('')
+    const [isAuthenticated, setIsAuthenticated] = useState(true) //? why would we START with true?
  
   useEffect(() => {
-    
+    let token;
+    if (!localStorage.getItem('jwt-token')){
+        setIsAuthenticated(false)
+        console.log(`============> not authenticated`)
+    }else{
+        token = jwt_decode(localStorage.getItem('token'))
+        setAuthToken(localStorage.getItem('token'))
+        setCurrentUser(token)
+    }
   }, []);
+
+    const nowCurrentUser = (userData) => {
+        console.log("===============> nowCurrentUser running")
+        setCurrentUser(userData)
+        setIsAuthenticated(true)
+    }
+
+    const handleLogout = () => {
+        if (localStorage.getItem('jwt-token')){
+            ///remove token from localStorage
+            localStorage.removeItem('jwt-token')
+            setCurrentUser(null)
+            setIsAuthenticated(false)
+        }
+    }
 
   return (
     <div className="App">
       <h1>MERN Authentication</h1>
-      <Profile />
+      <Navbar handleLogout={handleLogout} isAuth={isAuthenticated}/>
+      <div className={"container mt-5"}>
+          <Switch>
+              <Route path={"/signup"} component={Signup} />
+              <Route
+                path={"/login"}
+                render={(props)=> <Login {...props}
+                nowCurrentUser={nowCurrentUser}
+                setIsAuthenticated={setIsAuthenticated}
+                user={currentUser}
+                />}
+              />
+              <PrivateRoute path={"/profile"} component={Profile} user={currentUser} handleLogout={handleLogout} />
+              <Route exact path={"/"} component={Welcome} />
+              <Route path={"/about"} component={About} />
+          </Switch>
+      </div>
+        <Footer />
     </div>
   );
 }
